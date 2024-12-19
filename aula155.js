2492681937292ebd1fe2893cfcbc73b6a45b2b69
Cleber Paracampos Sardá <cleber@suportesc.com.br>
2024-12-18 23:56:42 -0300

    CFB Cursos

--- aula155.js
+++ aula155.js
@@ -61,14 +61,39 @@ const dgv=(configdgv)=>{
             const imgEditar=document.createElement("img");
             imgEditar.setAttribute("class","dgvIcone");
             imgEditar.setAttribute("src","editar.svg");
+            imgEditar.addEventListener("click",(evt)=>{
+                document.querySelector("#janelaEditar").classList.remove("ocultar");
+                const id=evt.target.parentNode.parentNode.firstChild.innerHTML;
+                const endpoint=`http://127.0.0.1:8080/produto/${id}`;
+                fetch(endpoint)
+                .then(res=>res.json())
+                .then(res=>{
+                    document.querySelector("#f_ideditar").value=res[0].n_id_produto;
+                    document.querySelector("#f_produtoeditar").value=res[0].s_nome_produto;
+                    document.querySelector("#f_marcaeditar").value=res[0].s_marca_produto;
+                    document.querySelector("#f_modeloeditar").value=res[0].
+                    S_modelo_produto;
+                }) 
+            });
             c5.appendChild(imgEditar);
+
 
             const imgExibir=document.createElement("img");
             imgExibir.setAttribute("class","dgvIcone");
             imgExibir.setAttribute("src","exibir.svg");
             imgExibir.addEventListener("click",(evt)=>{
+                document.querySelector(".janelaView").classList.remove("ocultar");
                 const id=evt.target.parentNode.parentNode.firstChild.innerHTML;
-                const endpoint=`http://127.0.0.1:8080/exibirprodutos/${id}`;
+                const endpoint=`http://127.0.0.1:8080/produto/${id}`;
+                fetch(endpoint)
+                .then(res=>res.json())
+                .then(res=>{
+                    document.querySelector("#f_id").value=res[0].n_id_produto;
+                    document.querySelector("#f_produto").value=res[0].s_nome_produto;
+                    document.querySelector("#f_marca").value=res[0].s_marca_produto;
+                    document.querySelector("#f_modelo").value=res[0].
+                    S_modelo_produto;
+                })   
             });
             c5.appendChild(imgExibir);
 
@@ -79,3 +104,30 @@ const dgv=(configdgv)=>{
 
 dgv(configdgv);
 
+
+document.querySelector("#btn_ok").addEventListener("click",(evt)=>{
+    document.querySelector(".janelaView").classList.add("ocultar");
+});
+document.querySelector("#btn_gravar").addEventListener("click",(evt)=>{
+    document.querySelector("#janelaEditar").classList.add("ocultar");
+    const id=document.querySelector("#f_ideditar").value;
+    const produto=document.querySelector("#f_produtoeditar").value;
+    const marca=document.querySelector("#f_marcaeditar").value;
+    const modelo=document.querySelector("#f_modeloeditar").value;
+    const endpoint=`http://127.0.0.1:8080/updateproduto/${id}/${produto}/${marca}/${modelo}`;
+    fetch(endpoint)
+    .then(res=>{
+        if(res.status==200){
+            document.querySelector("#janelaEditar").classList.add("ocultar");
+            dgv(configdgv);
+        }else{
+            alert("Erro ao Atualizar");
+        }
+    })
+
+    
+});
+
+document.querySelector("#btn_cancelar").addEventListener("click",(evt)=>{
+    document.querySelector("#janelaEditar").classList.add("ocultar");
+});
