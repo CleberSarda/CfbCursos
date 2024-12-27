e40fa4b841cc5947c0ecc74a38fe1b128533b59a
Cleber Paracampos Sardá <cleber@suportesc.com.br>
2024-12-26 23:00:14 -0300

    Cfb Cursos (Agenda)

--- agenda/js/pesquisar.js
+++ agenda/js/pesquisar.js
@@ -2,10 +2,18 @@ const btn_pesq=document.querySelector("#btn_pesq");
 const f_txtpesq=document.querySelector("#f_txtpesq")
 
 btn_pesq.addEventListener("click",(evt)=>{
-    if(f_txtpesq.value==""){
+    const valorpesq=f_txtpesq.value;
+    if(valorpesq==""){
         alert("digite a pesquisa");
         f_txtpesq.focus();
         return;
     }
-    console.log(document.getElementsByName("f_pesq").value);
+    const f_pesq=document.querySelector("input[name=f_pesq]:checked").value;
+    const endpoint=`http://127.0.0.1:8080/pesquisarcontatos/${f_pesq}/${valorpesq}`;
+    fetch(endpoint)
+    .then(res=>res.json())
+    .then(res=>{
+        console.log(res);
+    })
+   
 });
