9e7c39eb7905759436786e62b2a6759df8aa3dd5
Cleber Paracampos Sardá <cleber@suportesc.com.br>
2025-01-05 01:12:27 -0300

    CFB Cursos (Estoque)

--- estoque/backoffice/js/index.js
+++ estoque/backoffice/js/index.js
@@ -1,42 +1,13 @@
-const cabecalho=document.querySelector("#cabecalho")
-const menu=document.querySelector("#menu")
-const btn_home=document.querySelector("#btn_home")
-const btn_novo=document.querySelector("#btn_novo")
-const btn_pesquisar=document.querySelector("#btn_pesquisar")
-const btn_filtrar=document.querySelector("#btn_filtrar")
-const btn_gestao=document.querySelector("#btn_gestao")
-const btn_sobre=document.querySelector("#btn_sobre")
-const principal=document.querySelector("#principal")
+const btn_menuPrincipal=document.querySelector("#btn_menuPrincipal");
+const menuPrincipal=document.querySelector("#menuPrincipal");
+const todosMenusPrincipais=[...document.querySelectorAll(".btn_menuItem")];
 
-btn_home.addEventListener("click",(evt)=>{
-    abrirPagina(evt.target,"./home.html");
+btn_menuPrincipal.addEventListener("click",(evt)=>{
+    menuPrincipal.classList.toggle("ocultar");
 });
 
-btn_novo.addEventListener("click",(evt)=>{
-    abrirPagina(evt.target,"./novo.html");
-});
-
-btn_pesquisar.addEventListener("click",(evt)=>{
-    abrirPagina(evt.target,"./pesquisar.html");
-});
-
-btn_filtrar.addEventListener("click",(evt)=>{
-    abrirPagina(evt.target,"./filtrar.html");
-});
-
-btn_gestao.addEventListener("click",(evt)=>{
-    abrirPagina(evt.target,"./gestao.html");
-});
-    
-btn_sobre.addEventListener("click",(evt)=>{
-    abrirPagina(evt.target,"./sobre.html");
-});
-
-const abrirPagina=(el,url)=>{
-    const abas=[...document.querySelectorAll(".aba")];
-    abas.forEach(e=>{
-        e.classList.remove("abaSelecionada");
+todosMenusPrincipais.forEach(e=>{
+    e.addEventListener("click",(evt)=>{
+        menuPrincipal.classList.add("ocultar");
     });
-    el.classList.add("abaSelecionada");
-    window.open(url,"if_principal");
-}
+});
