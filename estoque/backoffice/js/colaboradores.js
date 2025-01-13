e55ba665e78e9c93409c5dba76146aac5d0215c7
Cleber Paracampos Sardá <cleber@suportesc.com.br>
2025-01-13 20:30:53 -0300

    CFB Cursos (estoque)

---
+++ estoque/backoffice/js/colaboradores.js
@@ -0,0 +1,13 @@
+const btn_menuPrincipal=document.querySelector("#btn_menuPrincipal");
+const menuPrincipal=document.querySelector("#menuPrincipal");
+const todosMenusPrincipais=[...document.querySelectorAll(".btn_menuItem")];
+
+btn_menuPrincipal.addEventListener("click",(evt)=>{
+    menuPrincipal.classList.toggle("ocultar");
+});
+
+todosMenusPrincipais.forEach(e=>{
+    e.addEventListener("click",(evt)=>{
+        menuPrincipal.classList.add("ocultar");
+    });
+});
